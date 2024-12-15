dae8c91698eefc6fc3b209b1528f757397d0b91a
ojp13 <32867061+ojp13@users.noreply.github.com>
2024-12-15 22:05:50 +1300

    more testing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Camperison' });
+  res.render('index', { title: 'Oscar Town' });
 });
 
 module.exports = router;
